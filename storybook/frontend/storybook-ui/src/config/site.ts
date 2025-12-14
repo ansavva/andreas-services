a6fb1fd7192a7bd7b7adc0e91e44ce11d530b550
Andreas Savva <asavva1991@gmail.com>
2025-12-14 18:52:36 -0500

    Rebrand aillusion as storybook (#11)

--- storybook/frontend/storybook-ui/src/config/site.ts
+++ storybook/frontend/storybook-ui/src/config/site.ts
@@ -1,8 +1,8 @@
 export type SiteConfig = typeof siteConfig;
 
 export const siteConfig = {
-  name: "Aillusion",
-  description: "Be anything you want to be.",
+  name: "Storybook",
+  description: "AI portrait studio - upload photos, train models, and generate new images.",
   navItems: [
     {
       label: "Home",
